a5b93a5d49825e41ad9238cbff15f215afa3fcc0
ryan-mant <ryan.tuglife@hotmail.com>
2025-07-09 11:33:38 -0300

    up changes 0306

---
+++ src/test/java/org/example/FoobarTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FoobarTest {
+    @Test
+    void test(){
+//        fail("not yet implemented");
+        System.out.println("test");
+    }
+
+}
