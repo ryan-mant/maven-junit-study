7d78093ed952a26eff2acfc61663aec89f772379
ryan-mant <ryan.tuglife@hotmail.com>
2025-07-09 12:20:17 -0300

    add challenges of 0310 class

--- src/test/java/org/example/math/SimpleMathTest.java
+++ src/test/java/org/example/math/SimpleMathTest.java
@@ -16,7 +16,71 @@ class SimpleMathTest {
         
         assertEquals(expected, actual, () -> "%s + %s did not produce %s".formatted(firstNumber, secondNumber, expected));
         assertNotEquals(10.2D, actual, "The testSum() produce a incorrect result");
-        assertEquals(expected, actual, "The testSum() produce an null result");
+        assertNotNull(actual, "The testSum() produce an null result");
     }
+
+    @Test
+    void testSubtraction(){
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 5.2D;
+        double secondNumber = 2D;
+        Double actual = math.subtraction(firstNumber, secondNumber);
+        double expected = 3.2D;
+
+        assertEquals(expected, actual, () -> "%s - %s did not produce %s".formatted(firstNumber, secondNumber, expected));
+        assertNotEquals(10.2D, actual, "The testSum() produce a incorrect result");
+        assertNotNull(actual, "The testSum() produce an null result");
+    }
+
+    @Test
+    void testMultiplication(){
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 4.6D;
+        double secondNumber = 2.0D;
+        Double actual = math.multiplication(firstNumber, secondNumber);
+        double expected = 9.2D;
+
+        assertEquals(expected, actual, () -> "%s * %s did not produce %s".formatted(firstNumber, secondNumber, expected));
+        assertNotEquals(8D, actual, "The testSum() produce a incorrect result");
+        assertNotNull(actual, "The testSum() produce an null result");
+    }
+
+    @Test
+    void testDivision(){
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 3D;
+        double secondNumber = 3D;
+        Double actual = math.division(firstNumber, secondNumber);
+        double expected = 1D;
+
+        assertEquals(expected, actual, () -> "%s / %s did not produce %s".formatted(firstNumber, secondNumber, expected));
+        assertNotEquals(3D, actual, "The testSum() produce a incorrect result");
+        assertNotNull(actual, "The testSum() produce an null result");
+    }
+    @Test
+    void testMean(){
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 4D;
+        double secondNumber = 8D;
+        Double actual = math.mean(firstNumber, secondNumber);
+        double expected = 6D;
+
+        assertEquals(expected, actual, () -> "%s + %s / 2 did not produce %s".formatted(firstNumber, secondNumber, expected));
+        assertNotEquals(10.2D, actual, "The testSum() produce a incorrect result");
+        assertNotNull(actual, "The testSum() produce an null result");
+    }
+    @Test
+    void testSquareRoot(){
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 121D;
+        Double actual = math.squareRoot(firstNumber);
+        double expected = 11D;
+
+        assertEquals(expected, actual, () -> "SquareRoot of %s did not produce %s".formatted(firstNumber, expected));
+        assertNotEquals(10.2D, actual, "The testSum() produce a incorrect result");
+        assertNotNull(actual, "The testSum() produce an null result");
+    }
+
+
 
 }
