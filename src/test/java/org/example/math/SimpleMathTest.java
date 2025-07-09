86c4b6e5f5a9efca599bb33aec45c345c0fc139f
ryan-mant <ryan.tuglife@hotmail.com>
2025-07-09 12:01:54 -0300

    refac with variable and arrow function, 0309

--- src/test/java/org/example/math/SimpleMathTest.java
+++ src/test/java/org/example/math/SimpleMathTest.java
@@ -9,10 +9,12 @@ class SimpleMathTest {
     @Test
     void testSum() {
         SimpleMath math = new SimpleMath();
-        Double actual = math.sum(6.2D, 2D);
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+        Double actual = math.sum(firstNumber, secondNumber);
         double expected = 8.2D;
         
-        assertEquals(expected, actual, "The testSum() did not produce expected result");
+        assertEquals(expected, actual, () -> "%s + %s did not produce %s".formatted(firstNumber, secondNumber, expected));
         assertNotEquals(10.2D, actual, "The testSum() produce a incorrect result");
         assertEquals(expected, actual, "The testSum() produce an null result");
     }
